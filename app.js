da482cca6fa143612967af175a5ff352079d558b
And-Bain <find@A5-004029.localdomain>
2018-11-07 17:22:55 -0700

    started javascript

---
+++ app.js
@@ -0,0 +1,55 @@
+// var sum = 1 + 2
+
+ alert(sum)
+/*
+multi line comment
+*/
+//order of operation
+ console.log(1 + 2  * 3 + (9 - 2))
+ var diff = 1 - 2
+ var product = 1 * 2
+ var divide = 1 / 2
+
+ console.log(diff)
+ console.log(product)
+ console.log(divide)
+
+ var string = 'hello'
+ var string2 = 'world'
+
+ console.log(string +string2)
+ console.log(string + 1)
+ adding 2 stings = concatination 
+// = assignment
+// == equality
+// integer whole number 1, 2, 17
+// float decimal numbers 1.2, 12.0, 13.55
+// boolean true or false 1 0
+// arrays colection of objects
+//[]
+// [1, 2, 3, 4]
+//['hi', 'hello', 'hey']
+//['hey', 1, 1930]
+
+function
+ function sayHello() {
+    console.log('hello')
+ }
+ sayHello()
+ var num1 = 2
+ var num2 = 12
+ function product(x, y) {
+    console.log(x * y)
+ }
+ product(num1, num2)
+ function btnClick() {
+     alert('lyay you touched me')
+ }
+ function mouseOver() {
+     console.log('you touched me!')
+ }
+
+ var mysuperarr = [[1, 3], ['hi', 'we'], [1.23, true]]
+ console.log(mysuperarr[2])
+
+ console.log(mysuperarr.length)
